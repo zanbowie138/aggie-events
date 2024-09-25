40aa626103ffd67ba452a8c6306ceb30b2981f0d
DunsinK <133840905+DunsinK@users.noreply.github.com>
2024-09-24 19:29:53 -0500

    added something intersting to home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,5 +3,7 @@ import React from 'react'
 export default function Page() {
     return (<>
         <h1 className="text-3xl">Aggie Events Homepage!</h1>
+        <p> something intersting</p>
+        
     </>)
 }
