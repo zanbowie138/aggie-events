8b4cc501949cdc5eab17cc79eb5b94ba7ec5fe72
Alex Bui <zanbowie138@gmail.com>
2024-09-24 19:57:35 -0500

    Add calendar page

---
+++ frontend/app/calendar/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Calendar() {
+    return (<>
+        <h1 className="text-3xl">This is a calendar page.</h1>
+        <h2>This is not a calendar.</h2>
+    </>)
+}
