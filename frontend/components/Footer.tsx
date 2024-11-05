b531c4637cedec861782d434f6e209916de64286
Angela Yue <angela.yue.2006@gmail.com>
2024-11-05 15:14:41 -0600

    footer design changes

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -1,28 +1,26 @@
-import React from 'react'
-
-
+import React from 'react';
 
 export default function Footer() {
   return (
-      <footer className="bg-maroon text-white mt-auto border-t-4 border-black">
-        {/* Navigation links */}
-        <nav>
-          <div className="flex">
+    <footer style={{ backgroundColor: '#000000' }} className="text-gray-400 mt-auto border-t-4 border-gray-700">
+      {/* Navigation links */}
+      <nav>
+        <div className="flex">
+          {/* Add your navigation links here if needed */}
+        </div>
+      </nav>
 
-          </div>
-        </nav>
-
-        {/* Copyright */}
-        <div>
+      {/* Copyright */}
+      <div>
         <div className="flex items-center justify-between w-[92%] mx-auto p-5">
-            <div>
-              <p>© 2021 Aggie Events</p>
-            </div>
-            <div>
-              <p>Created by: Aggie Events Team</p>
-            </div>
+          <div>
+            <p className="montserrat-light">ALL RIGHTS RESERVED © 2021 </p>
+          </div>
+          <div>
+            <p className="montserrat-light">AGGIEEVENTS TEAM</p>
           </div>
         </div>
-      </footer>
-  )
+      </div>
+    </footer>
+  );
 }
