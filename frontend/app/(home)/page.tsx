aa326de8e87ad2fee4ef9fab33cd35d63f51fff9
Angela Yue <angela.yue.2006@gmail.com>
2024-11-05 14:53:26 -0600

    goofy color changes to home page

--- frontend/app/(home)/page.tsx
+++ frontend/app/(home)/page.tsx
@@ -8,18 +8,30 @@ export default function Homepage() {
   return (
     <>
       <div className="relative w-full h-screen flex items-center justify-center overflow-hidden">
-        <HomeHeader />
-        <div className="p-5 md:pl-14 md:pt-14 text-center">
-          <div className="cormorant-garamond-bold text-white mt-2 text-xl">
-            <AnimText baseText={TypingTextBase} texts={TypingText} delay={0} />
-          </div>
-          <div className="w-fit pt-5 mx-auto">
-            <hr className="border-white w-3/4 mx-auto" />
-            <h2 className="cormorant-garamond-light-italic mt-2 text-xl max-w-xs">
-              Howdy! Welcome to AggieEvents, the one stop shop for events and organizations happening on campus!
-            </h2>
-          </div>
-        </div>
+  <HomeHeader />
+  <div className="p-5 md:pl-14 md:pt-14 text-center">
+    <div className="cormorant-garamond-bold text-white mt-2 text-xl">
+      <AnimText baseText={TypingTextBase} texts={TypingText} delay={0} />
+    </div>
+    <div className="w-fit pt-5 mx-auto">
+      <hr className="w-3/4 mx-auto" />
+      <h2 className="cormorant-garamond-light-italic mt-2 text-xl max-w-xs">
+      Howdy! Welcome&nbsp;to&nbsp;
+        <span 
+          style={{ 
+            color: '#CE3B4A', 
+            textShadow: '0 0 5px rgba(206, 59, 74, 0.8), 0 0 10px rgba(206, 59, 74, 0.6), 0 0 15px rgba(206, 59, 74, 0.4)' 
+          }} 
+          className="font-bold"
+        >
+    &nbsp;AggieEvents
+    </span>,&nbsp;
+        the one stop shop for events and organizations happening on campus!
+      </h2>
+    </div>
+  </div>
+
+
 
         <div className="absolute bottom-5 w-full">
           <div className="flex justify-center gap-x-20 mt-4"> {/* Added margin for spacing */}
