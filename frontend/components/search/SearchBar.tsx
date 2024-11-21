2ea5c129d16d043609d3d7c211249a109e22e204
Alex Bui <zanbowie138@gmail.com>
2024-11-20 21:54:41 -0600

    slight bg darken with search bar effect

--- frontend/components/search/SearchBar.tsx
+++ frontend/components/search/SearchBar.tsx
@@ -1,12 +1,15 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { FaSearch } from "react-icons/fa";
 
 export default function SearchBar() {
   const [searchInput, setSearchInput] = useState<string>("");
   const searchParams = useSearchParams();
+  const [focused, setFocused] = useState<boolean>(false);
+  const searchRef = useRef<HTMLFormElement>(null);
   const { push } = useRouter();
+  const inputRef = useRef<HTMLInputElement>(null);
 
   function handleSearch() {
     // Manipulate url query parameters
@@ -19,34 +22,66 @@ export default function SearchBar() {
     push(`/search?${params.toString()}`);
   }
 
+  useEffect(() => {
+    const handleClickOutside = (event: any) => {
+      if (searchRef.current && !searchRef.current.contains(event.target)) {
+        setFocused(false);
+      }
+    };
+
+    // Add event listener when the menu is open
+    if (focused) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+      inputRef.current?.blur();
+    }
+
+    // Cleanup event listener on component unmount
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [focused]);
+
   return (
-    <form className="flex grow justify-center hover:drop-shadow-lg">
-      <input
-        id="search-input"
-        alt="Test"
-        className="text-black bg-gray-100 rounded-l-md px-2 py-1 max-w-[700px]
-            w-full h-10 outline-none
-            focus:border-[#202020] focus:border-y-2 focus:border-l-2 peer"
-        placeholder="Search..."
-        onChange={(e) => setSearchInput(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
+    <>
+      <form
+        className={`flex grow justify-center hover:drop-shadow-lg ${focused && "z-[999]"}`}
+        ref={searchRef}
+      >
+        <input
+          id="search-input"
+          alt="Test"
+          className="text-black bg-gray-100 rounded-l-md px-2 py-1 max-w-[700px]
+            w-full h-10 outline-none"
+          placeholder="Search..."
+          ref={inputRef}
+          onChange={(e) => setSearchInput(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              e.preventDefault();
+              handleSearch();
+            }
+            if (e.key === "Escape") {
+              setFocused(false);
+            }
+          }}
+          onClick={() => setFocused(true)}
+        />
+        <button
+          className="bg-maroon rounded-r-md py-2 px-3"
+          onClick={(e) => {
             e.preventDefault();
             handleSearch();
-          }
-        }}
-      />
-      <button
-        className="bg-maroon rounded-r-md py-2 px-3
-          peer-focus:border-[#202020] peer-focus:border-y-2 peer-focus:border-r-2"
-        onClick={(e) => {
-          e.preventDefault();
-          handleSearch();
-          // TODO: fix this to update the tags on the first popstate
-        }}
-      >
-        <FaSearch color="white" />
-      </button>
-    </form>
+            // TODO: fix this to update the tags on the first popstate
+          }}
+        >
+          <FaSearch color="white" />
+        </button>
+      </form>
+      {focused && (
+        <div className="fixed h-screen w-screen bg-black/20 top-0 left-0 z-[998]"></div>
+      )}
+    </>
   );
 }
